819ee7c9f02fc7bd1a0a738d44edad26303e60a8
Winnerhust <910894904@qq.com>
2014-01-15 22:00:01 +0800

    Create stack.h

---
+++ stack.h
@@ -0,0 +1,33 @@
+#ifndef _EULER_STACK_H
+#define _EULER_STACK_H
+/*
+empty() 堆栈为空则返回真 
+pop() 移除栈顶元素 
+push() 在栈顶增加元素 
+size() 返回栈中元素数目 
+top() 返回栈顶元素 
+*/
+#ifndef SIZE_T
+#define SIZE_T
+typedef unsigned long size_t;
+#endif
+
+#define INIT_STACK_SIZE 256
+class Stack
+{
+public:
+	Stack();
+	~Stack();
+
+	bool empty() const;
+	void pop();
+	void push(void *elem);
+	size_t size() const;
+	void *top();
+private:
+	void ** data;
+	size_t len;
+	size_t capacity;
+};
+
+#endif
