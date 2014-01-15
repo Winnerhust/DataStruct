b483d7de075d4c5df0a733a57044cd34a3791a0e
Winnerhust <910894904@qq.com>
2014-01-15 22:00:20 +0800

    Create test_queue.cpp

---
+++ test_queue.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+#include "queue.h"
+void test_push()
+{
+	cout<<"--------test push--------"<<endl;
+	Queue q;
+	int a=3,b=5,c=2;
+	
+	q.push(&a);
+	q.push(&b);
+	q.push(&c);
+
+	assert(q.size()==3);
+	assert(*(int *)q.front()==3);
+	assert(*(int *)q.back()==2);
+
+	cout<<"test OK"<<endl;
+}
+void test_pop()
+{
+	cout<<"--------test pop--------"<<endl;
+	Queue q;
+	int a=3,b=5,c=2;
+	
+	q.push(&a);
+	q.push(&b);
+	q.push(&c);
+
+	assert(q.size()==3);
+	assert(*(int *)q.front()==3);
+	assert(*(int *)q.back()==2);
+	assert(!q.empty());
+
+	q.pop();
+	assert(q.size()==2);
+	assert(*(int *)q.front()==5);
+	assert(*(int *)q.back()==2);
+	assert(!q.empty());
+
+	cout<<"test OK"<<endl;
+}
+
+int main()
+{
+	test_push();
+	test_pop();
+	return 0;
+}
