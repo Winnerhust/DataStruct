d3a31644ff664afe9dab17d50d4240a1e90904ba
Winnerhust <910894904@qq.com>
2014-01-15 21:58:36 +0800

    Create queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,79 @@
+#ifndef _EULER_QUEUE_CPP
+#define _EULER_QUEUE_CPP
+
+#include <assert.h>
+#include <stdio.h>
+#include "queue.h"
+
+Queue::Queue(): head(NULL)	,tail(NULL),len(0)
+{
+}
+
+Queue::~Queue()
+{
+	Qlist *p =head,*q = head;
+
+	while(p){
+		q = p;
+		delete p;
+		p = q->next;
+	}
+}
+
+void *Queue::back()
+{
+	if( tail)
+		return tail->data;
+
+	return NULL;
+}
+void *Queue::front()
+{
+	if( head)
+		return head->data;
+
+	return NULL;
+}
+
+bool Queue::empty()const
+{
+	return (len==0);
+}
+
+size_t Queue::size() const
+{
+	return len;
+}
+
+void Queue::pop()
+{
+	Qlist *p = head;
+	if(p){
+		head = head ->next;
+		delete p;
+
+		--len;
+	}
+}
+
+void Queue::push(void * elem)
+{
+	Qlist *p = new Qlist();
+	assert(p);
+	
+	p->data = elem;
+	p->next = NULL;
+	
+	++len;
+
+	if(head==NULL){
+		head = p;
+		tail = head;
+	}else{
+		tail->next = p;
+		tail = p;
+	}
+}
+
+#endif
+
